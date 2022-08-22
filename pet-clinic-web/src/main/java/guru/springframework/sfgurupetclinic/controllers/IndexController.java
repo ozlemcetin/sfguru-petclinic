68e529fcc6dfa0bcd3602de1af29119a5d9cf029
ozlemcetin <ozlem.cetin@gmail.com>
2022-08-22 22:27:40 +0300

    Add Pet Clinic Index Page and Controller Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgurupetclinic/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgurupetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(path = {"", "/", "index", "index.html"})
+    public String showIndexPage() {
+        return "index";
+    }
+}
