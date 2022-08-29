ac295844457f428c6c8c623b259ce847708222a3
ozlemcetin <ozlem.cetin@gmail.com>
2022-08-29 15:35:22 +0300

    Create Vet Speciality Entity, Associate To Vet Closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgurupetclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgurupetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    /*
+    ===
+     */
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
