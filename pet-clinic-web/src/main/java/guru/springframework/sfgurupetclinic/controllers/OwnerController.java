1259a2f6bc976ad6e4bf815bccc75eb417d2679f
ozlemcetin <ozlem.cetin@gmail.com>
2022-10-23 16:36:14 +0300

    Web Data Binder Closes #62

--- pet-clinic-web/src/main/java/guru/springframework/sfgurupetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgurupetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package guru.springframework.sfgurupetclinic.controllers;
 import guru.springframework.sfgurupetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +19,12 @@ public class OwnerController {
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
 
     /*
         http://localhost:8080/owners
